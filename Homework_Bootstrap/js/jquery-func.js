165a3cbcc6d25ccacb3e6088a0025d489155fe13
coolmancz <a686868520@gmail.com>
2016-06-22 15:56:56 +0000

    add Homework_Bootstrap to github

---
+++ Homework_Bootstrap/js/jquery-func.js
@@ -0,0 +1,6 @@
+jQuery(function ($) {
+    //=================================== scroll  ===================================//
+    $('.navbar-toggle').click(function(){
+        $('.collapse').toggle();
+    });
+});
